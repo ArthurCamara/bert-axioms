333cc7c9e1298c8d9eaf563cef905a29e133db5b
Arthur Camara <camara.arthur@gmail.com>
2019-08-19 19:08:12 +0200

    prints

--- scripts/sentence_level_classifier.py
+++ scripts/sentence_level_classifier.py
@@ -1,13 +1,15 @@
 from msmarco_dataset import MsMarcoDataset
-from pytorch_transformers import BertForNextSentencePrediction, AdamW, WarmupLinearSchedule
+from pytorch_transformers import (BertForNextSentencePrediction,
+                                  AdamW, WarmupLinearSchedule)
 import random
 import numpy as np
 import torch
 import logging
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
-import yappi
 import multiprocessing
+from sklearn.metrics import f1_score
+
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +38,7 @@ def init_optimizer(
 
 def fine_tune(
         dataset: MsMarcoDataset,
+        data_dir,
         seed: int = 42,
         limit_gpus: int = -1,
         bert_model="bert-base-uncased",
@@ -65,7 +68,7 @@ def fine_tune(
     if n_workers is None:
         n_workers = multiprocessing.cpu_count() - 2
     data_loader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=True, num_workers=0 )
+        dataset, batch_size=batch_size, shuffle=True, num_workers=0)
     num_train_optimization_steps = len(data_loader) // n_epochs
 
     optimizer, scheduler = init_optimizer(
@@ -94,9 +97,75 @@ def fine_tune(
             outputs = model(**inputs)
             loss = outputs[0]
             print(loss)
+            if n_gpu > 1:
+                loss = loss.mean()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            tr_loss += loss.item()
+            scheduler.step()
+            optimizer.step()
+            model.zero_grad()
+            global_step += 1
+            if global_step % 50 == 0:
+                results = evaluate(dataset,
+                                   data_dir,
+                                   model,
+                                   device,
+                                   n_workers=n_workers)
+
+
+def evaluate(eval_dataset: MsMarcoDataset,
+             output_dir: str,
+             model: BertForNextSentencePrediction,
+             device: str,
+             task_name="msmarco",
+             prefix="",
+             eval_batchsize=32,
+             n_workers=2):
+    results = {}
+    eval_dataloader = DataLoader(
+        eval_dataset, batch_size=eval_batchsize, shuffle=False, num_workers=n_workers)
+    eval_loss = 0.0
+    nb_eval_steps = 0
+    preds = None
+    out_label_ids = None
+    for batch in tqdm(eval_dataloader, desc="Evaluating"):
+        model.eval()
+        batch = tuple(t.to(device) for t in batch)
+        with torch.no_grad():
+            inputs = {
+                'input_ids': batch[0],
+                'attention_mask': batch[1],
+                'token_type_ids': batch[2],
+                'next_sentence_label': batch[3]
+            }
+            outputs = model(**inputs)
+            tmp_eval_loss, logits = outputs[:2]
+            eval_loss += tmp_eval_loss.mean().item()
+        nb_eval_steps += 1
+        if preds is None:
+            preds = logits.detach().cpu().numpy()
+            out_label_ids = inputs['next_sentence_label'].detach().cpu().numpy()
+        else:
+            preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+            out_label_ids = np.append(
+                out_label_ids, inputs['next_sentence_label'].detach().cpu().numpy(), axis=0)
+        eval_loss = eval_loss / nb_eval_steps
+        preds = np.argmax(preds, axis=1)
+        assert len(preds) == len(out_label_ids)
+        result = {}
+        result["acc"] = (preds == out_label_ids).mean()
+        result["f1"] = f1_score(y_true=out_label_ids, y_pred=preds)
+        result["acc_and_f1"] = (result["acc"] + result["f1"]) / 2
+        results.update(result)
+
+        
+            
 
 
 if __name__ == "__main__":
-    dataset = MsMarcoDataset(
-        "/ssd2/arthur/TREC2019/data/small_sample.tsv", "/ssd2/arthur/TREC2019/data")
-    fine_tune(dataset)
+    train_dataset = MsMarcoDataset(
+        "/ssd2/arthur/TREC2019/data/small_sample.tsv",
+        "/ssd2/arthur/TREC2019/data")
+    fine_tune(train_dataset, "/ssd2/arthur/TREC2019/data")
