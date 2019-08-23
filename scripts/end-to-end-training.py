80f2cef8097f9e879280379764b2dda51fc19e3d
Arthur Camara <camara.arthur@gmail.com>
2019-08-23 13:14:50 +0200

    encapsulated arguments

--- scripts/end-to-end-training.py
+++ scripts/end-to-end-training.py
@@ -4,8 +4,10 @@ import argparse
 import logging
 import subprocess
 import os
+import sys
 from msmarco_dataset import MsMarcoDataset
-from TREC_run_to_BERT import trec_run_to_bert
+from trecrun_to_bert import TRECrun_to_BERT
+from args_parser import getArgs
 
 
 def get_path(home_dir, x):
@@ -40,33 +42,24 @@ def run_retrieval_step(data_dir, k, anserini_path, overwrite=False):
 
     logging.info("Run file for train has %d lines", line_counter_train)
     # Generated BERT-Formatted triples
-    trec_run_to_bert(output_path, data_dir, 'train', line_counter_train, k)
+    TRECrun_to_BERT(output_path, data_dir, 'train', line_counter_train, k)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str,
-                        required=True, help="Data home")
-    parser.add_argument("--k", type=int, default=100,
-                        help="Top-k for reranking")
-    parser.add_argument("--log_level", type=str, default="INFO",
-                        help="Logging level. Defaults to INFO")
-    parser.add_argument("--anserini_path", type=str,
-                        default="/ssd2/arthur/Anserini/anserini.jar", help="anserini.jar path")
-    parser.add_argument("--overwrite", action="store_true",
-                        help="Force overwrite of everything")
-    parser.add_argument("--run_retrieval", action="store_true",
-                        help="Should the retrieval step run?")
-
-    args = parser.parse_args()
+    print(sys.argv)
+    args = getArgs(sys.argv[1:])
 
     logging.basicConfig(level=logging.getLevelName(args.log_level))
 
     if args.run_retrieval:
-        run_retrieval_step(args.data_dir, args.k, args.anserini_path, args.overwrite)
-    
+        run_retrieval_step(args.data_dir, args.k,
+                           args.anserini_path, args.overwrite)
+
     # Dataset loader
-    train_path = get_path(args.data_dir, "tiny_sample.tsv")
+    if args.train_file is not None:
+        train_path = args.train_file
+    else:
+        train_path = get_path(args.data_dir, "tiny_sample.tsv")
     train_dataset = MsMarcoDataset(train_path, args.data_dir)
 
     # Fine tune
