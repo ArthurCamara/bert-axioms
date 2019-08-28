ea69046a34cde2c43a8474b28959f2d8780a57bd
Arthur Camara <a.barbosacamara@tudelft.nl>
2019-08-28 10:07:12 +0200

    fine tune evaluation segmentation fault

--- scripts/bert_finetune.sh
+++ scripts/bert_finetune.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --partition=wis
+#SBATCH --partition=general
 #SBATCH --qos=long
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=16384
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16384
 #SBATCH --gres=gpu:4
 #SBATCH --mail-type=END
 
@@ -20,4 +20,3 @@ PYTHON=/home/nfs/acamara/venvs/bert/bin/python
 cd $SCRIPT_DIR
 
 srun $PYTHON end-to-end-training.py --data_dir $DATA_DIR --train_file $DATA_DIR/train-triples.top100 --dev_file $DATA_DIR/dev-triples.top100 --bert_model bert-base-uncased
-
